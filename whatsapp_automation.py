7ac8e81e6b1a611c0c2b486bcf2aa2a7f17042cf
AKASH PRAJAPATI <143386413+AKAS7991@users.noreply.github.com>
2024-12-02 22:03:43 +0530

    Updated whatsapp_automation.py

--- whatsapp_automation.py
+++ whatsapp_automation.py
@@ -9,6 +9,6 @@ x=int(input("Enter Number Of Messages. "))
 msg = input("Enter Message. ")
 y=0
 while x!=y:
-    driver.find_element(By.XPATH, "//*[@tabindex='10']").send_keys("Ram Ram")
+    driver.find_element(By.XPATH, "//*[@tabindex='10']").send_keys(msg)
     driver.find_element(By.XPATH, "//*[@data-icon='send']").click()
     y+=1
